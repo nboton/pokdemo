85899115e6e66a9ef9e639cf74c582345bfa56d8
nboton <botonoliv@gmail.com>
2021-02-12 00:38:52 +0100

    Creating pokemon service and adding httpservice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
 import { FormsModule } from '@angular/forms';
 import { FilterPokemonPipePipe } from './filter-pokemon--pipe.pipe';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { FilterPokemonPipePipe } from './filter-pokemon--pipe.pipe';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ], 
   providers: [],
   bootstrap: [AppComponent]
--- src/app/filter-pokemon--pipe.pipe.ts
+++ src/app/filter-pokemon--pipe.pipe.ts
@@ -1,13 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filterPokemonPipe'
+  name: 'filterPokemonPipe', pure:false
 })
 export class FilterPokemonPipePipe implements PipeTransform {
 
-  transform(value: any[], property?: string, searchString?: string): any {
-    if (typeof value !== 'undefined') {
-      return value.filter((e) => {
+  /**
+* 
+* @param value la fonction prend en entree un tableau sur lequel il doit filtrer (value:any[])
+* @param property un premier argument qui est la propriétéproperty?: string
+* @param searchString une donnée qui est qu'est elle est la valeur qu'on va rechercher dans le tableau
+*/
+  transform(value: any[], property: string, searchString: string): any {
+    if (typeof value !== 'undefined') {//si le tableau de valeur n'est pas vide, on va filtrer sur
+      return value.filter((e) => { //disant en ne regardant ceux dont la propriété de cette objet e[propriete] en ne passant à tout leur
         return e[property].toLowerCase().indexOf(searchString.toLowerCase()) !== -1;
       });
     } else {
